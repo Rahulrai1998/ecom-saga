59f1e5d91af20477b4fb8a2c494685487296acea
RahulTechanzo <rahulrai2822march@gmail.com>
2025-11-11 23:59:33 +0530

    api

--- src/store/productAction.js
+++ src/store/productAction.js
@@ -2,5 +2,10 @@ import { PRODUCT_LIST } from "./constant";
 
 export const listProduct = (product) => {
   console.log("Product added to cart", product);
-  return { type: PRODUCT_LIST, data: "hello" };
+
+  const data = fetch("https://jsonplaceholder.typicode.com/posts/1")
+    .then((response) => response.json())
+    .then((json) => console.log(json));
+  console.log(data);
+  return { type: PRODUCT_LIST, data};
 };
