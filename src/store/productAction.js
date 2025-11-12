92703778dcc60c51223ab4a6352f6e35b063146a
RahulTechanzo <rahulrai2822march@gmail.com>
2025-11-12 23:59:40 +0530

    api call

--- src/store/productAction.js
+++ src/store/productAction.js
@@ -1,7 +1,6 @@
 import { PRODUCT_LIST } from "./constant";
 
-export const listProduct = async (product) => {
-  let response = await fetch("https://jsonplaceholder.typicode.com/posts/1");
-  const data = response.json();
-  return { type: PRODUCT_LIST, data };
+export const listProduct = (product) => {
+ 
+  return { type: PRODUCT_LIST, data: product };
 };
--- src/store/productSaga.js
+++ src/store/productSaga.js
@@ -1,3 +1,14 @@
-function* productSaga() {}
+import { takeEvery } from "redux-saga/effects";
+import { PRODUCT_LIST } from "./constant";
+
+function* getProducts() {
+  let response = yield call(fetch, "http://localhost:3000/products");
+  const data = yield response.json();
+  console.log("API into saga", data);
+}
+
+function* productSaga() {
+  yield takeEvery(PRODUCT_LIST, getProducts);
+}
 
 export default productSaga;
